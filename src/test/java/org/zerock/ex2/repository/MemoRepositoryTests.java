d3a8cd3e48f40aae7b000328960c22380b1a536c
SuHyun <jish2689@gmail.com>
2021-09-10 23:15:50 +0900

    Paging 실습, 쿼리메소드 및 @Query 학습

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.zerock.ex2.entity.Memo;
 
@@ -17,6 +21,7 @@ public class MemoRepositoryTests {
     @Autowired
     MemoRepository memoRepository;
 
+    /*
     @Test
     public void testClass(){
         System.out.println(memoRepository.getClass().getName());
@@ -64,6 +69,41 @@ public class MemoRepositoryTests {
         memoRepository.deleteById(100L);
     }
 
+     */
+
+    @Test
+    public void testPageDefault(){
+
+        //1페이지 10개
+
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("-----------------------------------------------");
+        System.out.println("Total Pages: " + result.getTotalPages()); // 총 페이지 수
+        System.out.println("Total Count: " + result.getTotalElements() ); // 전체 데이터 개수
+        System.out.println("Page Number: " + result.getNumber()); // 현재 페이지 번호 0부터 시작
+        System.out.println("Page Size: " + result.getSize()); // 페이지당 데이터 개수
+        System.out.println("has next page? " + result.hasNext()); // 다음 페이지 존재 여부
+        System.out.println("first page? " + result.isFirst()); // 시작페이지(0) 여부
+                
+    }
+
+
+    @Test
+    public void testSort(){
+
+
+        Sort sort1 = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+
+        result.get().forEach(memo ->{
+            System.out.println(memo);
+        });
+
+    }
 
 
 }
