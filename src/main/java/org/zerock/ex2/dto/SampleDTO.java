6a147ff6d62615ee89ad056fb75d74637391ef19
SuHyun <jish2689@gmail.com>
2021-09-13 21:22:37 +0900

    Thymeleaf 반복문, inline, 링크, 시간

---
+++ src/main/java/org/zerock/ex2/dto/SampleDTO.java
@@ -0,0 +1,17 @@
+package org.zerock.ex2.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime; // 현재 시각 표현 위해
+
+}
